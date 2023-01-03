9846508d46d55fd874506504cff0225cbc2f4ff2
kevin-r-m <kmclaughlinbtb@gmail.com>
2023-01-02 20:58:26 -0600

    added error handling and organized file

--- scripts/createComponent.js
+++ scripts/createComponent.js
@@ -1,39 +1,54 @@
 import { existsSync, mkdirSync, writeFileSync } from "node:fs";
 import promptSync from "prompt-sync";
 const prompt = promptSync();
-const { log } = console;
-
-// node includes script pathing so slice arguments at 2nd index
-log("<== Let's get a new component set-up ==>\n");
-const fileNameInput = prompt("Component name: ");
-
-function handleComponentCreation() {
-  checkArgs();
-  buildFileStructure();
-  writeFileSync(
-    `components/${fileNameInput}/${fileNameInput}.jsx`,
-    constructTemplate()
-  );
-  writeFileSync(`components/${fileNameInput}/${fileNameInput}.scss`, "");
-}
+const { log, error } = console;
 
 handleComponentCreation();
 
-function checkArgs() {
-  const args = process.argv.slice(2);
-  if (args.includes("help")) {
-    // serve usage text
-    log("included help");
+function handleComponentCreation() {
+  let fileNameInput;
+
+  log("<== Let's get a new component set-up ==>\n");
+
+  try {
+    fileNameInput = prompt("Component name: ");
+    if (!fileNameInput) {
+      throw "!== component name must be a valid value ==!";
+    }
+  } catch (e) {
+    error(e);
+    return;
   }
+
+  const parentPath = `components/${fileNameInput}/`;
+  const stylePath = `components/${fileNameInput}/${fileNameInput}.scss`;
+  const componentPath = `components/${fileNameInput}/${fileNameInput}.jsx`;
+
+  try {
+    buildFileStructure(parentPath, fileNameInput);
+  } catch (e) {
+    error(e);
+    return;
+  }
+
+  try {
+    writeFileSync(componentPath, constructTemplate(fileNameInput));
+    writeFileSync(stylePath, "");
+  } catch (e) {
+    error(e);
+    return;
+  }
+
+  log(`<== Component has been set-up, check ${parentPath}  ==>\n`);
 }
 
-function buildFileStructure() {
+function buildFileStructure(parentPath, fileNameInput) {
   if (!existsSync("components")) {
     mkdirSync("components");
   }
 
-  if (!existsSync(`components/${fileNameInput}/`)) {
-    mkdirSync(`components/${fileNameInput}/`);
+  if (!existsSync(parentPath)) {
+    mkdirSync(parentPath);
   } else {
     log(
       `${fileNameInput} already exists, please try again with a unique component name`
@@ -41,7 +56,7 @@ function buildFileStructure() {
   }
 }
 
-function constructTemplate() {
+function constructTemplate(fileNameInput) {
   const fileBody = {
     imports: [
       `import react from "react";\n`,
